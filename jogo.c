8094d3abe50b0702803e238381128178eaf3552c
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-07-21 19:54:55 -0300

    While
    Estrutura de repetição while

--- jogo.c
+++ jogo.c
@@ -1,41 +1,43 @@
 #include <stdio.h>  /* Permite o printf */
 #include <locale.h> /* Permite caracteres especiais (alterar UTF-8 para ISO 8859-1 no vs code) */
 
-#define TENTATIVAS 5 /* Diretiva (constante) */
-
 int main()
 {
     setlocale(LC_ALL, "Portuguese_Brazil"); /* Permite caracteres especiais */
 
-    printf("--------------------------------------\n");
+    printf("------------------------------------\n");
     printf("Boas vindas ao jogo de adivinhação!\n");
-    printf("Você deverá tentar acertar o número que\no computador pensou em %d tentativas.", TENTATIVAS);
-    printf("--------------------------------------\n");
+    printf("Você deverá tentar acertar o número\nque o computador pensou.\n");
+    printf("------------------------------------\n");
 
     int numSecreto = 42;
     printf("(O numero secreto é %d)\n", numSecreto);
 
     int chute;
-    for (int i = 1; i <= TENTATIVAS; i++)
-    {
-        printf("Qual é o seu %d° chute?: ", i);
+    int tentativas = 1;
+
+    while (1) {
+        printf("\nQual é sua %d° tentativa?: ", tentativas);
         scanf("%d", &chute);
 
-        if (chute == numSecreto)
-        {
-            printf("Parabéns! Você acertou o número secreto!");
+        if (chute < 0) {
+            printf("Você não pode chutar números negativos.\n");
+            continue;
+        }
+
+        if (chute == numSecreto) {
+            printf("Parabéns! Você acertou o número secreto!\n");
             break;
+
+        } else if (chute > numSecreto) {
+            printf("Uma pena, seu chute é maior que o número secreto... Mas você pode tentar novamente!\n");
+        } else {
+            printf("Uma pena, seu chute é menor que o número secreto... Mas você pode tentar novamente!\n");
         }
-        else if (chute > numSecreto)
-        {
-            printf("Uma pena, seu chute é maior que o número secreto... Mas você pode tentar novamente!");
-        }
-        else
-        {
-            printf("Uma pena, seu chute é menor que o número secreto... Mas você pode tentar novamente!");
-        }
+        
+        tentativas++;
     }
 
-    printf("Fim de jogo. Suas tentativas acabaram!");
+    printf("\nFim de jogo. Você acertou em %d tentativa(s)!", tentativas);
     return 0;
 }
