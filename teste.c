99e3d11142d4a8446974417bbf3b5aebd249563a
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-07-21 18:17:17 -0300

    Hello World!
    Aprendendo a imprimir strings no terminal

---
+++ teste.c
@@ -0,0 +1,9 @@
+#include <stdio.h> /*  */
+
+int main () 
+{
+    printf("-----------------------------------\n");
+    printf("Boas vindas ao jogo de adivinhar!\n");
+    printf("-----------------------------------\n");
+    return 0;
+}
